cd3ef0d6aecbaf27bbc8b65d7dd4f6cfbb4e2ae5
Elkhwaga <mohamedelkhwaga9@gmail.com>
2022-04-13 00:46:49 +0200

    new 20

--- udagram-frontend/src/karma.conf.js
+++ udagram-frontend/src/karma.conf.js
@@ -3,17 +3,36 @@
 
 module.exports = function (config) {
   config.set({
-    basePath: "../",
-    frameworks: ["jasmine"],
-    files: ["lib/angular.js", "app/**/*.js", "test/**/*.js"],
-    exclude: [],
-    reporters: ["progress"],
+    basePath: "",
+    frameworks: ["jasmine", "@angular-devkit/build-angular"],
+    plugins: [
+      require("karma-jasmine"),
+      require("karma-chrome-launcher"),
+      require("karma-jasmine-html-reporter"),
+      require("karma-coverage-istanbul-reporter"),
+      require("@angular-devkit/build-angular/plugins/karma"),
+    ],
+    client: {
+      clearContext: false, // leave Jasmine Spec Runner output visible in browser
+    },
+    coverageIstanbulReporter: {
+      dir: require("path").join(__dirname, "../coverage"),
+      reports: ["html", "lcovonly", "text-summary"],
+      fixWebpackSourcePaths: true,
+    },
+    browsers: ["Chrome"],
+    customLaunchers: {
+      ChromeHeadlessCI: {
+        base: "ChromeHeadless",
+        flags: ["--no-sandbox"],
+      },
+    },
+    reporters: ["progress", "kjhtml"],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
     browsers: ["Chrome"],
-    captureTimeout: 60000,
     singleRun: false,
   });
 };
