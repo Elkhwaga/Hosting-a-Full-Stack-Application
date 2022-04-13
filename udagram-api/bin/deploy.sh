32e403080147c593ea648360d0e624e8e7a30753
Elkhwaga <mohamedelkhwaga9@gmail.com>
2022-04-14 00:34:59 +0200

    new 45

--- udagram-api/bin/deploy.sh
+++ udagram-api/bin/deploy.sh
@@ -1,5 +1,15 @@
-eb init udagram-api2 -r $AWS_DEFAULT_REGION -p node.js
+eb init udagram-api2 -r $AWS_REGION -p node.js
 eb use udagram-api-dev
 eb deploy
 
 eb setenv AWS_REGION=$AWS_DEFAULT_REGION
+eb setenv AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID 
+eb setenv AWS_BUCKET=$AWS_BUCKET 
+eb setenv AWS_REGION=$AWS_DEFAULT_REGION 
+eb setenv SECRET_ACCESS_KEY_ID=$AWS_SECRET_ACCESS_KEY 
+eb setenv JWT_SECRET=$JWT_SECRET 
+eb setenv POSTGRES_DB=$POSTGRES_DB 
+eb setenv POSTGRES_HOST=$POSTGRES_HOST
+eb setenv POSTGRES_PASSWORD=$POSTGRES_PASSWORD 
+eb setenv POSTGRES_PORT=$POSTGRES_PORT 
+eb setenv POSTGRES_USERNAME=$POSTGRES_USERNAME
