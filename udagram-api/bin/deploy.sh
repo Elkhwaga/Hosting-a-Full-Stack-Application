f7ce2ec79fc7e87ddaec21e9c0cc19303051e17d
Elkhwaga <mohamedelkhwaga9@gmail.com>
2022-04-14 06:18:52 +0200

    new 60

--- udagram-api/bin/deploy.sh
+++ udagram-api/bin/deploy.sh
@@ -1,5 +1,9 @@
-eb init udagram-api -r $AWS_DEFAULT_REGION -p node.js
-eb use Udagramapi-env
-eb deploy
+# eb init udagram-api -r $AWS_DEFAULT_REGION -p node.js
+# eb use Udagramapi-env
+# eb deploy
 
-eb setenv AWS_REGION=$AWS_DEFAULT_REGION
+eb init udagram-api --region us-east-1
+eb use Udagramapi-env
+eb deploy Udagramapi-env
+
+# eb setenv AWS_REGION=$AWS_DEFAULT_REGION
