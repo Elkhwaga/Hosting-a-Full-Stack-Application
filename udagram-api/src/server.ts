c7519135a16897c5b92bd36d53e5b48a43cf8cdb
Elkhwaga <mohamedelkhwaga9@gmail.com>
2022-04-14 05:01:54 +0200

    new 58

--- udagram-api/src/server.ts
+++ udagram-api/src/server.ts
@@ -18,7 +18,7 @@ import { V0_FEED_MODELS, V0_USER_MODELS } from "./controllers/v0/model.index";
   console.log("Database Connected");
 
   const app = express();
-  const port = 8080;
+  const port = process.env.PORT || 8080;
 
   app.use(bodyParser.json());
 
@@ -28,7 +28,7 @@ import { V0_FEED_MODELS, V0_USER_MODELS } from "./controllers/v0/model.index";
 
   // Root URI call
   app.get("/", async (req, res) => {
-    res.send("/api/v0/");
+    res.send("Welcome");
   });
 
   // Start the Server
